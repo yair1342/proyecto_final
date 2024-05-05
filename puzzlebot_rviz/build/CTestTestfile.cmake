998e864c740986b58ec5a9754ea7c6469485e2b4
yair1342 <A01707962@tec.mx>
2024-05-04 20:46:16 -0600

    eliminar cosas inecesarias

--- puzzlebot_rviz/build/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/chris/catkin_ws/src/puzzlebot_sim
-# Build directory: /home/chris/catkin_ws/src/puzzlebot_sim/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("gtest")
--- puzzlebot_rviz/build/catkin_generated/installspace/puzzlebot_sim-msg-extras.cmake
+++
@@ -1,2 +0,0 @@
-set(puzzlebot_sim_MESSAGE_FILES "")
-set(puzzlebot_sim_SERVICE_FILES "")
